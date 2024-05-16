ffb1c1fc7f542ab37bffbd59edda7ea990e354a7
GabrielLaxy <gablaxycarreira@gmail.com>
2024-05-16 11:15:22 -0300

    atualizacoes em aula

--- arvores/TesteABB.java
+++ arvores/TesteABB.java
@@ -3,9 +3,9 @@ import java.util.Random;
 public class TesteABB {
     public static void main(String[] args) {
         Random random = new Random();
-        int[] v = new int[200];
+        int[] v = new int[12];
         ABB abb = new ABB();
-        for (int i=0; i<200; i++) {
+        for (int i=0; i<12; i++) {
             v[i] = random.nextInt(20) + 1;
             System.out.print(v[i] + " ");
             abb.insere(v[i]);
@@ -13,5 +13,8 @@ public class TesteABB {
         System.out.println("\na arvore:\n" + abb.stringInOrder());
         System.out.println("\nnivel: " + abb.nivel());
         System.out.println("\ntotal de nos: " + abb.numeroNos());
+        System.out.println("\nMaior valor: " + abb.maior());
+        System.out.println("\nMaior por recursao: " + abb.chamaMaiorRec());
+        System.out.println("\nTemos " + abb.folhas() + " folhas");
     }
 }
